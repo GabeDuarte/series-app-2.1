f8b8a0d71aa637dad3b0405e48339f307e15e674
GabeDuarte <83724430+GabeDuarte@users.noreply.github.com>
2021-08-13 15:05:19 -0300

    att

--- src/series.js
+++ src/series.js
@@ -1,196 +1,92 @@
 import React from "react";
+
 import "./inicio.css";
+
 import { Link } from "react-router-dom";
 
-
-// function magica(){
-//   var $element = document.getElementById('editando')
-
-//   if ($element.hasAttribute('ativo')){
-//       $element.removeAttribute('ativo')
-//       $element.style.display='none';
-//   }
-//   else{
-//     $element.setAttribute('ativo', 'true')
-//     $element.style.display='block'
-//   }
-// }
 
 
 export default class Series extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      series: [
-        {
-          id:'1',
-          nome: "The Big Bang Theory",
-          imagem_url:
-            "https://s2.glbimg.com/476xiyzHwobzomhiO6QZ8ZaZPCM=/362x536/https://s2.glbimg.com/gw3G9SRdfhWZbNxnMuwbznGiAW0=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/s/Y/z9FwyXQjq1flWRIm3mUA/2020-726-series-warner-the-big-bang-theory-poster.jpg",
-          ano_lancamento: "2007",
-          qtd_temp: "12",
-        },
-        {
-            id:'2',
-          nome: "gabriel duarte",
-          imagem_url:
-            "https://s2.glbimg.com/476xiyzHwobzomhiO6QZ8ZaZPCM=/362x536/https://s2.glbimg.com/gw3G9SRdfhWZbNxnMuwbznGiAW0=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/s/Y/z9FwyXQjq1flWRIm3mUA/2020-726-series-warner-the-big-bang-theory-poster.jpg",
-          ano_lancamento: "2007",
-          qtd_temp: "12",
-        },
-        {
-            id:'3',
-          nome: "Friends",
-          imagem_url: "https://br.web.img3.acsta.net/pictures/19/12/20/21/27/3055482.jpg",
-          ano_lancamento: "1994",
-          qtd_temp: "10",
-        },
-        {
-            id:'4',
-          nome: "Sex Education",
-          imagem_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxgnY2O8FTQtRqSDJNZpDlYMysAW5i7Z59V1jbbazOQRBKyXHaVRg8gA3wY123eihrTAU&usqp=CAU",
-          ano_lancamento: "2019",
-          qtd_temp: "3",
-        },
-        {
-            id:'5',
-          nome: "Breaking Bad",
-          imagem_url: "https://presleyson.com.br/wp-content/uploads/2018/12/breaking-bad-800x800-min.png?a7c535&a7c535",
-          ano_lancamento: "2008",
-          qtd_temp: "5",
-        },
-        {
-            id:'6',
-          nome: "Breaking Bad",
-          imagem_url: "https://presleyson.com.br/wp-content/uploads/2018/12/breaking-bad-800x800-min.png?a7c535&a7c535",
-          ano_lancamento: "2008",
-          qtd_temp: "5",
-        },
-        {
-            id:'7',
-          nome: "Breaking Bad",
-          imagem_url: "https://presleyson.com.br/wp-content/uploads/2018/12/breaking-bad-800x800-min.png?a7c535&a7c535",
-          ano_lancamento: "2008",
-          qtd_temp: "5",
-        },
-        {
-            id:'8',
-          nome: "Breaking Bad",
-          imagem_url: "https://presleyson.com.br/wp-content/uploads/2018/12/breaking-bad-800x800-min.png?a7c535&a7c535",
-          ano_lancamento: "2008",
-          qtd_temp: "5",
-        },
-        {
-            id:'9',
-          nome: "Breaking Bad",
-          imagem_url: "https://presleyson.com.br/wp-content/uploads/2018/12/breaking-bad-800x800-min.png?a7c535&a7c535",
-          ano_lancamento: "2008",
-          qtd_temp: "5",
-        },
-        {
-            id:'10',
-          nome: "The Big Bang Theory",
-          imagem_url:
-            "https://s2.glbimg.com/476xiyzHwobzomhiO6QZ8ZaZPCM=/362x536/https://s2.glbimg.com/gw3G9SRdfhWZbNxnMuwbznGiAW0=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/s/Y/z9FwyXQjq1flWRIm3mUA/2020-726-series-warner-the-big-bang-theory-poster.jpg",
-          ano_lancamento: "2007",
-          qtd_temp: "12",
-        },
-        {
-            id:'11',
-          nome: "The Big Bang Theory",
-          imagem_url:
-            "https://s2.glbimg.com/476xiyzHwobzomhiO6QZ8ZaZPCM=/362x536/https://s2.glbimg.com/gw3G9SRdfhWZbNxnMuwbznGiAW0=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/s/Y/z9FwyXQjq1flWRIm3mUA/2020-726-series-warner-the-big-bang-theory-poster.jpg",
-          ano_lancamento: "2007",
-          qtd_temp: "12",
-        },
-      ],
-      id:'',
+      series: [],
+
       nomeSerie:'',
       imagem_urlSerie:'',
       ano_lancamentoSerie:'',
       qtd_tempSerie:'',
+      yt_linkSerie: '',
+      descriçaoSerie:'',
       editando: false,
       indexEditando: null,
     };
   }
 
-  onSubmit= (e) =>{
+  async buscarSeries(){
+    this.setState({loading: true})
+    const res = await fetch('http://localhost:5000/series');
+    const json = await res.json();
+    this.setState({series: json, loading:false});
+  }
+
+  componentDidMount() {
+    this.buscarSeries()
+  }
+
+  onSubmit= async (e) =>{
     e.preventDefault();
 
-    const {series, editando, indexEditando, idSerie, nomeSerie,  imagem_urlSerie, qtd_tempSerie, ano_lancamentoSerie } = this.state;
+    const {series, editando, idEditando, yt_linkSerie, descriçaoSerie, nomeSerie,  imagem_urlSerie, qtd_tempSerie, ano_lancamentoSerie } = this.state;
 
     if (editando){
-      const seriesAtualizadas = series.map((serie, index) => {
-        if (indexEditando === index){
-          serie.id = idSerie;
-          serie.nome = nomeSerie;
-          serie.ano_lancamento = ano_lancamentoSerie;
-          serie.imagem_url = imagem_urlSerie;
-          serie.qtd_temp = qtd_tempSerie;
-
-        }
-        return serie;
-
-      });
+      await fetch(`http://localhost:5000/series/${idEditando}`, {
+        method: "PUT",
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({nome: nomeSerie, imagem_url: imagem_urlSerie, ano_lancamento: ano_lancamentoSerie, qtd_temp: qtd_tempSerie, yt_link: yt_linkSerie, descriçao: descriçaoSerie})
+      }); 
+      this.buscarSeries();
+      
       this.setState({
-        series: seriesAtualizadas,
         indexEditando: null,
         editando: false,
       });
 
     } else {
-      this.setState({
-        series: [
-          ...series,
-          {
-            id: idSerie,
-            nome: nomeSerie,
-            ano_lancamento: ano_lancamentoSerie,
-            imagem_url: imagem_urlSerie,
-            qtd_temp: qtd_tempSerie
-          },
-        ]
-      });
+      await fetch('http://localhost:5000/series', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({ 
+          nome: nomeSerie, imagem_url: imagem_urlSerie, ano_lancamento: ano_lancamentoSerie, qtd_temp: qtd_tempSerie, yt_link: yt_linkSerie
+        })
+      })
     };
     this.setState({
-      idSerie:'',
       nomeSerie: '',
       imagem_urlSerie:'',
       qtd_tempSerie: '',
-      ano_lancamentoSerie:''
+      ano_lancamentoSerie:'',
+      yt_link: '',
+      descriçao: '',
+
     });
   };
 
-  deletar =(index) => {
-    const {series} = this.state;
-    this.setState({
-      series: series.filter((serie, i)=> i!== index),
-    })
+  deletar = async (idDeletar) =>{
+    await fetch (`http://localhost:5000/series/${idDeletar}`,{
+      method: 'DELETE'
+    });
+    this.buscarSeries();
   };
 
 
   render(){
-    const {series,} = this.state;
-    // const {series, idSerie,  nomeSerie, imagem_urlSerie, ano_lancamentoSerie, qtd_tempSerie, editando, indexEditando} = this.state;
+    // const {series,} = this.state;
+     const {series, nomeSerie, imagem_urlSerie, ano_lancamentoSerie, yt_linkSerie, descriçaoSerie, qtd_tempSerie, editando, indexEditando} = this.state;
     return (
     <html>
       <body>
-          {/* <div className='container-fluid'>
-            <div Classname='menuLateral' id='side'>
-                  <div>
-                      <div className='algumaCoisa'>
-                        <ul className="listaMenu">
-                          <li>
-                            <a href="#">Generos</a>
-                          </li>
-                          <li>
-                            <a href="#">alguma coisa</a>
-                          </li>
-                        </ul>
-                      </div>
-                  </div>
-              </div> 
-          </div>     */}
+
         <main className="main">
           <header>
             <div className="Cabecalho">
@@ -206,6 +102,50 @@ export default class Series extends React.Component {
           </header>
 
           <div className="conteudo">
+          <div className='editando'>
+                  <h2>
+                      {editando?`Editando:${series[indexEditando]?.nome}`:"Cadastrar uma nova serie"}
+                  </h2>
+                  <form onSubmit={this.onSubmit}>
+                      <input placeholder = 'nome' value={nomeSerie} onChange={(e)=>{
+                      this.setState({
+                          nomeSerie: e.target.value
+                      });
+                      }}/>
+                      <br></br>
+                      <input placeholder = 'URL da imagem' value={imagem_urlSerie} onChange={(e)=>{
+                      this.setState({
+                          imagem_urlSerie: e.target.value
+                      });
+                      }}/>
+                      <br></br>
+                      <input placeholder = 'Ano de lançamento' value={ano_lancamentoSerie} onChange={(e)=>{
+                      this.setState({
+                          ano_lancamentoSerie: e.target.value
+                      });
+                      }}/>
+                      <br></br>
+                      <input placeholder = 'Quantidade de temporadas' value={qtd_tempSerie} onChange={(e)=>{
+                      this.setState({
+                          qtd_tempSerie: e.target.value
+                      });
+                      }}/>
+                      <br></br>
+                      <input placeholder = 'Link do YouTube' value={yt_linkSerie} onChange={(e)=>{
+                      this.setState({
+                          yt_linkSerie: e.target.value
+                      });
+                      }}/>
+                      <br></br>
+                      <input placeholder = 'Descrição' value={descriçaoSerie} onChange={(e)=>{
+                      this.setState({
+                          descriçaoSerie: e.target.value
+                      });
+                      }}/>
+                      <br></br>
+                      <button type='submit'>enviar</button>
+                  </form>
+              </div>
              <div className='flex-container'>
                {series.map((s, index) => (
                  <div className="item">
@@ -213,12 +153,15 @@ export default class Series extends React.Component {
                    <img src={s.imagem_url} alt={s.nome}></img>
                    <h3>Ano de lançamento: {s.ano_lancamento}</h3>
                    <h3>Quantidade de temporadas: {s.qtd_temp}</h3>
+                   <iframe width="400" heigth='315' src={s.yt_link}></iframe>
+                   <p> descrição: {s.descriçao}</p>
                    <div className='botoes'>
-                    <button onClick={() => this.deletar(index)}>Deletar</button>
+                    <button onClick={() => this.deletar(s._id)}>Deletar</button>
                     <br/>
                     <button id='btn'
                     onClick={()=>{
                       this.setState({
+                        idEditando: s._id,
                         editando: true,
                         nomeSerie: s.nome,
                         imagem_urlSerie: s.imagem_url,
@@ -244,31 +187,4 @@ export default class Series extends React.Component {
     </html> 
     )
   }
-
-
-
-  // render() {
-  //   const { series } = this.state;
-  //   return (
-  //     <body>
-  //       <div className='title'><h1>Series</h1></div>
-  //       <div className="conteudo">
-  //         <main>
-  //           <div >
-  //             {series.map((serie) => (
-  //               <div className="item">
-  //                 <h2>Nome: {serie.nome}</h2>
-  //                 <img src={serie.imagem_url} alt={serie.nome}></img>
-  //                 <h3>Ano de lançamento: {serie.ano_lancamento}</h3>
-  //                 <h3>Quantidade de temporadas: {serie.qtd_temp}</h3>
-  //               </div>
-  //             ))}
-  //           </div>
-  //         </main>
-  //       </div>
-  //     </body>
-  //   );
-  // }
-
-
 }
